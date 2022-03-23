fe1bb7624eae07f6bfd52c09e795a6da1acf0fe6
TronnieZ <vuvanhongson@gmail.com>
2022-03-24 00:25:11 +0700

    thirt commit
    hihi

---
+++ Tinder/app/build/generated/ap_generated_sources/debug/out/com/example/tinder/databinding/ActivityMainBindingImpl.java
@@ -0,0 +1,95 @@
+package com.example.tinder.databinding;
+import com.example.tinder.R;
+import com.example.tinder.BR;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import android.view.View;
+@SuppressWarnings("unchecked")
+public class ActivityMainBindingImpl extends ActivityMainBinding  {
+
+    @Nullable
+    private static final androidx.databinding.ViewDataBinding.IncludedLayouts sIncludes;
+    @Nullable
+    private static final android.util.SparseIntArray sViewsWithIds;
+    static {
+        sIncludes = null;
+        sViewsWithIds = new android.util.SparseIntArray();
+        sViewsWithIds.put(R.id.toolbar1, 1);
+        sViewsWithIds.put(R.id.toolbar_title, 2);
+        sViewsWithIds.put(R.id.frame_layout, 3);
+        sViewsWithIds.put(R.id.bottom_navigation, 4);
+    }
+    // views
+    @NonNull
+    private final android.widget.LinearLayout mboundView0;
+    // variables
+    // values
+    // listeners
+    // Inverse Binding Event Handlers
+
+    public ActivityMainBindingImpl(@Nullable androidx.databinding.DataBindingComponent bindingComponent, @NonNull View root) {
+        this(bindingComponent, root, mapBindings(bindingComponent, root, 5, sIncludes, sViewsWithIds));
+    }
+    private ActivityMainBindingImpl(androidx.databinding.DataBindingComponent bindingComponent, View root, Object[] bindings) {
+        super(bindingComponent, root, 0
+            , (com.etebarian.meowbottomnavigation.MeowBottomNavigation) bindings[4]
+            , (android.widget.FrameLayout) bindings[3]
+            , (androidx.appcompat.widget.Toolbar) bindings[1]
+            , (android.widget.TextView) bindings[2]
+            );
+        this.mboundView0 = (android.widget.LinearLayout) bindings[0];
+        this.mboundView0.setTag(null);
+        setRootTag(root);
+        // listeners
+        invalidateAll();
+    }
+
+    @Override
+    public void invalidateAll() {
+        synchronized(this) {
+                mDirtyFlags = 0x1L;
+        }
+        requestRebind();
+    }
+
+    @Override
+    public boolean hasPendingBindings() {
+        synchronized(this) {
+            if (mDirtyFlags != 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean setVariable(int variableId, @Nullable Object variable)  {
+        boolean variableSet = true;
+            return variableSet;
+    }
+
+    @Override
+    protected boolean onFieldChange(int localFieldId, Object object, int fieldId) {
+        switch (localFieldId) {
+        }
+        return false;
+    }
+
+    @Override
+    protected void executeBindings() {
+        long dirtyFlags = 0;
+        synchronized(this) {
+            dirtyFlags = mDirtyFlags;
+            mDirtyFlags = 0;
+        }
+        // batch finished
+    }
+    // Listener Stub Implementations
+    // callback impls
+    // dirty flag
+    private  long mDirtyFlags = 0xffffffffffffffffL;
+    /* flag mapping
+        flag 0 (0x1L): null
+    flag mapping end*/
+    //end
+}
---
+++ Tinder/app/build/generated/data_binding_trigger/debug/com/example/tinder/DataBindingTriggerClass.java
@@ -0,0 +1,4 @@
+package com.example.tinder;
+
+@androidx.databinding.BindingBuildInfo
+public class DataBindingTriggerClass {}
